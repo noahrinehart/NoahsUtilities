59fa8941383ad0cc36570b4e2c6f3d645c4a0301
Noah Rinehart <rinehart.noah@gmail.com>
2014-12-30 14:46:56 -0800

    tried to make a file :(

--- app/src/main/java/nrinehart/io/noahsutilities/fragments/TagFragment.java
+++ app/src/main/java/nrinehart/io/noahsutilities/fragments/TagFragment.java
@@ -2,6 +2,7 @@ package nrinehart.io.noahsutilities.fragments;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,14 +12,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.mpatric.mp3agic.Mp3File;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
 
 import nrinehart.io.noahsutilities.R;
 
 public class TagFragment extends Fragment{
 
     Button selectButton;
+    ImageButton playButton;
     TextView curTrack;
     ImageView curImage;
     EditText curArtist;
@@ -29,6 +41,7 @@ public class TagFragment extends Fragment{
     EditText curComment;
     EditText curComposer;
     EditText curAlbumArtist;
+    Button saveButton;
 
     public final int READ_REQUEST_CODE = 42;
 
@@ -48,6 +61,7 @@ public class TagFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_tag, container, false);
 
         selectButton = (Button) view.findViewById(R.id.selectsong_button);
+        playButton = (ImageButton) view.findViewById(R.id.play_button);
         curImage = (ImageView) view.findViewById(R.id.current_image);
         curTrack = (TextView) view.findViewById(R.id.current_song);
         curArtist = (EditText) view.findViewById(R.id.current_artist);
@@ -58,13 +72,33 @@ public class TagFragment extends Fragment{
         curComment = (EditText) view.findViewById(R.id.current_comment);
         curComposer = (EditText) view.findViewById(R.id.current_composer);
         curAlbumArtist = (EditText) view.findViewById(R.id.current_albumartist);
+        saveButton = (Button) view.findViewById(R.id.save_button);
+
+
 
         selectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*//song.start();
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("audio/mpeg");
                 startActivityForResult(intent, READ_REQUEST_CODE);
+                MediaPlayer mp=MediaPlayer.create(getActivity(), R.raw.sandstorm);
+                mp.start();*/
+                Uri songUri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/raw/sandstorm.mp3");
+
+                File son = new File(songUri.getPath());
+                File newSong;
+                
+
+
+
+                Boolean existsBool = son.exists();
+                String exists = existsBool.toString();
+                Toast toast = Toast.makeText(getActivity().getApplicationContext(), exists, Toast.LENGTH_SHORT);
+                toast.show();
+
+
             }
         });
 
@@ -86,6 +120,13 @@ public class TagFragment extends Fragment{
 
         }
     }
+    @Override
+    public void onPause() {
+        super.onPause();
+       // song.stop();
+        //song.release();
+    }
+
 
 
 
