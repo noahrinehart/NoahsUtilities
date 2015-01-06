902289c54d50c77906591cc0da3250178f598915
Noah Rinehart <rinehart.noah@gmail.com>
2015-01-05 19:35:24 -0800

    fixed settings

--- app/src/main/java/nrinehart/io/noahsutilities/settings/SettingsFragment.java
+++ app/src/main/java/nrinehart/io/noahsutilities/settings/SettingsFragment.java
@@ -2,7 +2,10 @@ package nrinehart.io.noahsutilities.settings;
 
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import nrinehart.io.noahsutilities.R;
@@ -16,6 +19,25 @@ public class SettingsFragment extends PreferenceFragment{
 
         ListPreference listPreference = (ListPreference)findPreference("pref_unit");
         listPreference.setValueIndex(0);
+
+        final CheckBoxPreference checkBoxPreference = (CheckBoxPreference)findPreference("pref_uselocation");
+        final EditTextPreference editTextPreference = (EditTextPreference)findPreference("pref_zipcode");
+        checkBoxPreference.setChecked(true);
+        if (checkBoxPreference.isChecked()){
+            editTextPreference.setEnabled(false);
+        }
+        checkBoxPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                if (checkBoxPreference.isChecked()){
+                    editTextPreference.setEnabled(false);
+                }else if(!checkBoxPreference.isChecked()){
+                    editTextPreference.setEnabled(true);
+                }
+                return false;
+            }
+        });
     }
 
 }
